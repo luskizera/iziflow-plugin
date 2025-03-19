233f8db5ba2561dc4b7178d03ef29ffd8c37d245
luskizera <112437099+luskizera@users.noreply.github.com>
2025-03-18 21:05:09 -0300

    feat: adicionar novos componentes de UI, incluindo AspectRatio, Skeleton, Collapsible, Label, Separator, Input, Progress, Toaster, Checkbox, Slider e Badge

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,17 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-
-declare const figma: any;
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export const notify = (message: string) => {
-  parent.postMessage({
-    pluginMessage: {
-      type: 'console',
-      message: `[UI] ${message}`
-    }
-  }, '*');
-};

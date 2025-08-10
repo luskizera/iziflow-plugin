c7310f65edbec228e7eddbe53df0e599616a8615
luskizera <112437099+luskizera@users.noreply.github.com>
2025-08-09 22:53:29 -0300

    chore: update figma name view

--- figma.config.ts
+++ figma.config.ts
@@ -2,8 +2,8 @@ import type { FigmaConfig, PluginManifest } from "vite-figma-plugin/lib/types";
 import { version } from "./package.json";
 
 export const manifest: PluginManifest = {
-  name: "IziFlow V2", 
-  id: "com.luskizera.iziflow-v2", 
+  name: "IziFlow", 
+  id: "com.luskizera.iziflow", 
   api: "1.0.0",
   main: "code.js",
   ui: "index.html",
